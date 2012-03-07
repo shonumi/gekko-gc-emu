b034fd35c72265b7412db68678314b787584a3d1
ShizZy247 <ShizZy247@36332193-8412-8e5e-4a1b-12d1b2851199>
2012-03-07 03:56:00 +0000

    Added comment header to common.h
    git-svn-id: https://gekko-gc-emu.googlecode.com/svn/trunk@20 36332193-8412-8e5e-4a1b-12d1b2851199

--- src/common/src/common.h
+++ src/common/src/common.h
@@ -1,5 +1,26 @@
-// common.h
-// (c) 2005,2008 Gekko Team / Wiimu Project
+/*!
+ * Copyright (C) 2005-2012 Gekko Emulator
+ *
+ * \file    common.h
+ * \author  ShizZy <shizzy247@gmail.com>
+ * \date    2012-02-04
+ * \brief   Common header for using the common library
+ *
+ * \section LICENSE
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * Official project repository can be found at:
+ * http://code.google.com/p/gekko-gc-emu/
+ */
 
 #ifndef COMMON_COMMON_H_
 #define COMMON_COMMON_H_
@@ -211,5 +232,6 @@ __inline static u64 BSWAP64(u64 x)
   return (u64)(((u64)BSWAP32((u32)(x&0xffffffff)))<<32) | (BSWAP32((u32)(x>>32)));
 } 
 #endif
+
 
 #endif // COMMON_COMMON_H_
