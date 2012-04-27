fc723406ab0025db71e8a56c91772986bbca9db4
ShizZy247 <ShizZy247@gmail.com>
2012-04-27 18:42:27 -0400

    changed emuwindow default to SDL2 on windows, or use SDL1 if SDL2 is unavail (*nix)

--- src/video_core/src/video_core.cpp
+++ src/video_core/src/video_core.cpp
@@ -1,12 +1,12 @@
 /*!
  * Copyright (C) 2005-2012 Gekko Emulator
  *
- * \file    video_core.cpp
- * \author  ShizZy <shizzy247@gmail.com>
- * \date    2012-03-08
- * \brief   Main module for new video core
+ * @file    video_core.cpp
+ * @author  ShizZy <shizzy247@gmail.com>
+ * @date    2012-03-08
+ * @brief   Main module for new video core
  *
- * \section LICENSE
+ * @section LICENSE
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
@@ -28,7 +28,8 @@
 
 #include "video_core.h"
 #include "renderer_base.h"
-#include "video/emuwindow_sdl.h"
+#include "emuwindow/emuwindow_sdl.h"
+#include "emuwindow/emuwindow_glfw.h"
 #include "renderer_gl3/renderer_gl3.h"
 #include "renderer_gl2/renderer_gl2.h"
 #include "fifo.h"
@@ -38,13 +39,11 @@
 
 namespace video_core {
 
-
 RendererBase*   g_renderer;         ///< Renderer plugin
 SDL_Thread      *g_video_thread;
 
-
 int VideoThreadEntry(void *unused) {
-    EmuWindow_SDL* emu_window = new EmuWindow_SDL;
+    EmuWindow_GLFW* emu_window = new EmuWindow_GLFW;
     g_renderer = new RendererGL3();
     g_renderer->SetWindow(emu_window);
     g_renderer->Init();
@@ -54,7 +53,11 @@ int VideoThreadEntry(void *unused) {
 
 /// Start the video core
 void Start() {
+#if SDL_MAJOR_VERSION == 2
+    g_video_thread = SDL_CreateThread(VideoThreadEntry, NULL, NULL);
+#else
     g_video_thread = SDL_CreateThread(VideoThreadEntry, NULL);
+#endif
 
     if (g_video_thread == NULL) {
         LOG_ERROR(TVIDEO, "Unable to create thread: %s... Exiting\n", SDL_GetError());
