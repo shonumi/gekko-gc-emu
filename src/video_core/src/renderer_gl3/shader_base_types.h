3a705918dd8dbad12c415adff5aeabd47f2ce770
ShizZy247 <ShizZy247@gmail.com>
2012-07-20 01:11:10 -0400

    various changes to support texturing - NOTE this stuff is soon to be deprecated

--- src/video_core/src/renderer_gl3/shader_base_types.h
+++ src/video_core/src/renderer_gl3/shader_base_types.h
@@ -25,13 +25,15 @@
 #ifndef VIDEO_CORE_SHADER_BASE_TYPES_H_
 #define VIDEO_CORE_SHADER_BASE_TYPES_H_
 
-/// Default Vertex Shader
+// Default Vertex Shader
+//      NOTE: Vertex coords (position, normal, and texture) are sent with the last (third or fourth)
+//      coordinate as the shift value. It will always an 8-bit unsigned integer.
 const char kDefaultVertexShader[] = 
-    "#version 150\n"
+    "#version 330\n"
     "layout(location = 0) in vec3 position;\n"
     "layout(location = 1) in vec4 color0;\n"
     "layout(location = 2) in vec4 color1;\n"
-    "layout(location = 3) in vec3 normal;\n"
+    "layout(location = 3) in vec4 normal;\n"
     "layout(location = 4) in vec2 texcoord0;\n"
     "layout(location = 5) in vec2 texcoord1;\n"
     "layout(location = 6) in vec2 texcoord2;\n"
@@ -47,6 +49,7 @@ const char kDefaultVertexShader[] =
     "uniform vec4 modelview_vectors[3];\n"
     "uniform vec4 position_vectors[64];\n"
     "out vec4 vertexColor;\n"
+    "out vec2 vertexTexCoord0;\n"
     "mat4 convert_matrix(in vec4 v0, in vec4 v1, in vec4 v2) {\n"
     "    return mat4(v0[0], v1[0], v2[0], 0.0,\n"
     "                v0[1], v1[1], v2[1], 0.0,\n"
@@ -64,7 +67,8 @@ const char kDefaultVertexShader[] =
     "                                          modelview_vectors[1],\n"
     "                                          modelview_vectors[2]);\n"
     "    }\n"
-    "    gl_Position = projection_matrix * modelview_matrix * vec4(position, 1.0);\n"
+    "    gl_Position = projection_matrix * modelview_matrix * vec4(position.xyz, 1.0);\n"
+    "    vertexTexCoord0 = texcoord0.st;\n"
     "    vertexColor = vec4(color0[3]/255.0f, color0[2]/255.0f, color0[1]/255.0f,\n"
     "                                                           color0[0]/255.0f);\n"
     "}";
@@ -73,45 +77,61 @@ const char kDefaultVertexShader[] =
 
 /// Default Geometry Shader (Quads), otherwise disabled
 const char kDefaultGeometryShaderQuads[] =
-    "#version 150\n"
+    "#version 330\n"
     "precision highp float;\n"
     "layout (lines_adjacency) in;\n"
     "layout (triangle_strip) out;\n"
     "layout (max_vertices = 4) out;\n"
     "in vec4 vertexColor[];\n"
+    "in vec2 vertexTexCoord0[];\n"
     "out vec4 gsColor;\n"
+    "out vec2 gsTexCoord0;\n"
     "void main(void) {\n"
     "   gl_Position = gl_in[0].gl_Position;\n"
     "   gsColor = vertexColor[0];\n"
+    "   gsTexCoord0 = vertexTexCoord0[0].st;\n"
     "   EmitVertex();\n"
+
     "   gl_Position = gl_in[1].gl_Position;\n"
     "   gsColor = vertexColor[1];\n"
+    "   gsTexCoord0 = vertexTexCoord0[0].st;\n"
     "   EmitVertex();\n"
+
     "   gl_Position = gl_in[3].gl_Position;\n"
-    "   gsColor = vertexColor[2];\n"
-    "   EmitVertex();\n"
-    "   gl_Position = gl_in[2].gl_Position;\n"
     "   gsColor = vertexColor[3];\n"
+    "   gsTexCoord0 = vertexTexCoord0[3].st;\n"
     "   EmitVertex();\n"
+
+    "   gl_Position = gl_in[2].gl_Position;\n"
+    "   gsColor = vertexColor[2];\n"
+    "   gsTexCoord0 = vertexTexCoord0[2].st;\n"
+    "   EmitVertex();\n"
+
     "   EndPrimitive();\n"
     "}";
 
 /// Default fragment shader
 const char kDefaultFragmentShader[] =
-    "#version 150\n"
+    "#version 330\n"
+    "uniform sampler2D texture0;\n"
     "in vec4 vertexColor;\n"
+    "in vec2 vertexTexCoord0;\n"
     "out vec3 fragmentColor;\n"
     "void main() {\n"
-    "    fragmentColor = vertexColor.rgb;\n"
+    "    fragmentColor = texture(texture0, vertexTexCoord0.st).rgb;\n"
+    //"    fragmentColor = vertexColor.rgb;\n"
     "}\n";
 
 /// Default fragment shader for Quads
 const char kDefaultFragmentShaderQuads[] =
-    "#version 150\n"
+    "#version 330\n"
+    "uniform sampler2D texture0;\n"
     "in vec4 gsColor;\n"
+    "in vec2 gsTexCoord0;\n"
     "out vec3 fragmentColor;\n"
     "void main() {\n"
-    "    fragmentColor = gsColor.rgb;\n"
+    "    fragmentColor = texture(texture0, gsTexCoord0.st).rgb;\n"
+    //"    fragmentColor = gsColor.rgb;\n"
     "}\n";
 
 #endif // VIDEO_CORE_SHADER_BASE_TYPES_H_
