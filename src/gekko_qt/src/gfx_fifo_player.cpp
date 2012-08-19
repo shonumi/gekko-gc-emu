42c8bcaa3df6fb5fb2609c63230d009ade96dd61
neobrain <neobrainx@gmail.com>
2012-08-19 18:28:30 +0200

    gekko_qt: Implement most of the fifo player UI logic.

--- src/gekko_qt/src/gfx_fifo_player.cpp
+++ src/gekko_qt/src/gfx_fifo_player.cpp
@@ -1,11 +1,77 @@
+#include <QFileDialog>
+
 #include "gfx_fifo_player.hxx"
 
-GGfxFifoPlayerControl::GGfxFifoPlayerControl(QWidget* parent) : QDockWidget(parent)
+#include "fifo_player.h"
+
+GGfxFifoPlayerControl::GGfxFifoPlayerControl(QWidget* parent) : QDockWidget(parent), last_recording(NULL)
 {
     ui.setupUi(this);
+
+    connect(ui.startStopRecordingButton, SIGNAL(clicked()), this, SLOT(OnStartStopRecordingClicked()));
+    connect(ui.saveRecordingButton, SIGNAL(clicked()), this, SLOT(OnSaveRecordingClicked()));
+    connect(ui.startPlaybackButton, SIGNAL(clicked()), this, SLOT(OnStartPlaybackClicked()));
 }
 
 GGfxFifoPlayerControl::~GGfxFifoPlayerControl()
 {
+    fifo_player::EndRecording();
+}
 
+void GGfxFifoPlayerControl::OnStartStopRecordingClicked()
+{
+    if (!fifo_player::IsRecording())
+    {
+        ui.saveRecordingButton->setEnabled(false);
+        ui.playbackGroup->setEnabled(false);
+        ui.startStopRecordingButton->setText(tr("Stop"));
+        // TODO: Adjust parameters
+        fifo_player::StartRecording();
+    }
+    else
+    {
+        last_recording = &fifo_player::EndRecording();
+        // TODO: Do other stuff
+
+        ui.startStopRecordingButton->setText(tr("Start"));
+        ui.playbackGroup->setEnabled(true); // TODO: Shouldn't do that while playing back
+        ui.saveRecordingButton->setEnabled(true);
+    }
+}
+
+void GGfxFifoPlayerControl::OnSaveRecordingClicked()
+{
+    // TODO
+    QString filename = QFileDialog::getOpenFileName(this, tr("Save Fifo log"), QString(), QString());
+    if (filename.size())
+        fifo_player::Save(filename.toLatin1().data(), (fifo_player::FPFile&)*last_recording);
+    ui.playbackGroup->setEnabled(true);
+}
+
+void GGfxFifoPlayerControl::OnStartPlaybackClicked()
+{
+    switch (ui.playbackSourceCombobox->currentIndex())
+    {
+        case PLAYBACK_SOURCE_LAST_RECORDING:
+        {
+            fifo_player::PlayFile((fifo_player::FPFile&)*last_recording);
+            break;
+        }
+
+        case PLAYBACK_SOURCE_FROM_FILE:
+        {
+            QString filename = QFileDialog::getOpenFileName(this, tr("Save Fifo log"), QString(), QString());
+            if (filename.size())
+            {
+                fifo_player::FPFile file;
+                fifo_player::Load(filename.toLatin1().data(), file);
+                fifo_player::PlayFile(file);
+                break;
+            }
+            else return;
+        }
+    }
+
+    ui.playbackGroup->setEnabled(false);
+    // TODO: Re-enable playback group once playback is finished
 }
--- src/gekko_qt/src/gfx_fifo_player.hxx
+++ src/gekko_qt/src/gfx_fifo_player.hxx
@@ -4,6 +4,10 @@
 #include <QDockWidget>
 #include "ui_gfx_fifo_player.h"
 
+namespace fifo_player {
+    struct FPFile;
+}
+
 class GGfxFifoPlayerControl : public QDockWidget
 {
     Q_OBJECT
@@ -12,8 +16,21 @@ public:
     GGfxFifoPlayerControl(QWidget* parent = NULL);
     ~GGfxFifoPlayerControl();
 
+private slots:
+    void OnStartStopRecordingClicked();
+    void OnSaveRecordingClicked();
+
+    void OnStartPlaybackClicked();
+
 private:
+    enum {
+        PLAYBACK_SOURCE_LAST_RECORDING = 0,
+        PLAYBACK_SOURCE_FROM_FILE = 1,
+    };
+
     Ui::GfxFifoPlayerControl ui;
+
+    const fifo_player::FPFile* last_recording;
 };
 
 #endif // _GEKKO_GFX_FIFO_PLAYER_HXX_
