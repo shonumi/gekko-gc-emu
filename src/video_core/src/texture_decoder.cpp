e8d79cb6f1dad7ac471ce4bde4b3808b99f14e71
ShizZy <shizzy@6bit.net>
2012-12-05 21:48:46 -0500

    added missing docstring comments to texture_decoder.h and texture_decoder.cpp

--- src/video_core/src/texture_decoder.cpp
+++ src/video_core/src/texture_decoder.cpp
@@ -1,5 +1,26 @@
-// gx_texture.cpp
-// (c) 2005,2009 Gekko Team
+/**
+ * Copyright (C) 2005-2012 Gekko Emulator
+ *
+ * @file    texture_decoder.cpp
+ * @author  ShizZy <shizzy247@gmail.com>
+ * @date    2012-06-01
+ * @brief   Simple texture decoder for GameCube graphics texture formats
+ *
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * Official project repository can be found at:
+ * http://code.google.com/p/gekko-gc-emu/
+ */
 
 #include "sys/stat.h"
 #include "common.h"
--- src/video_core/src/texture_decoder.h
+++ src/video_core/src/texture_decoder.h
@@ -1,3 +1,27 @@
+/**
+ * Copyright (C) 2005-2012 Gekko Emulator
+ *
+ * @file    texture_decoder.h
+ * @author  ShizZy <shizzy247@gmail.com>
+ * @date    2012-06-01
+ * @brief   Simple texture decoder for GameCube graphics texture formats
+ *
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * Official project repository can be found at:
+ * http://code.google.com/p/gekko-gc-emu/
+ */
+
 #ifndef VIDEO_CORE_TEXTURE_DECODER_H_
 #define VIDEO_CORE_TEXTURE_DECODER_H_
 
